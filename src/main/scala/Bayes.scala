bc600004fc54d8b569580d26e2ebbd9c3001d36a
owain lewis <owain@owainlewis.com>
2013-04-07 18:19:15 +0100

    working basics

--- src/main/scala/Bayes.scala
+++ src/main/scala/Bayes.scala
@@ -43,21 +43,68 @@ trait Classifier {
     wordCount.put((category, word), 1 + current)
   }
 
-  def wordOccurs(category: String, word: String): Int = {
-    wordCount.get((category,word)) match {
-      case Some(v: Int) => v
-      case None => 0
+  /**
+   * The number of times a feature occurs in a category
+   *
+   */
+  def featureCount(feature: String, category: String): Double = {
+    wordCount.get((category,feature)) match {
+      case Some(v: Int) => v.toDouble
+      case None => 0.0
+    }
+  }
+
+  /**
+   * The number of times a category occurs
+   *
+   */
+  def catCount(category: String): Double = {
+    categoryCount.get(category) match {
+      case Some(v: Int) => v.toDouble
+      case None => 0.0
     }
   }
 }
 
 object Bayes extends Classifier {
 
-  def wordMetrics(word: String, category: String): Pair[Int, Int] =
-    ( wordOccurs(category, word), categoryCount(category) )
+  def wordMetrics(word: String, category: String): Pair[Double, Double] =
+    ( featureCount(word, category), catCount(category) )
 
-  def singleWordProbability(word: String, category: String)= {
-    wordMetrics(word, category)
+  /**
+   * The probability that an item is in a category
+   * P(C|D) => featureCount / categoryCount
+   */
+  def featureProbability(feature: String, category: String): Double = {
+    val metrics = wordMetrics(feature, category)
+    val featureCount = metrics._1
+    val categoryCount = metrics._2
+    if (categoryCount == 0) 0.0
+    else featureCount / categoryCount
+  }
+
+  /** The number of times a feature appears across ALL categories */
+  def featureTotals(feature: String): Int = {
+    val t = categoryCount flatMap {
+      case (k,_) => {
+        if(wordCount.contains((k, feature))) Some(wordCount((k, feature)))
+        else None
+      }}
+    t.sum
+  }
+
+  /** Calculate the weighted average probability */
+  def weightedAverage(weight: Double, assumed: Double, totals: Double, basicProbability: Double) = {
+    val x = (weight * assumed + totals * basicProbability)
+    val y = weight + totals
+    x / y
+  }
+
+  def weightedProbability(feature: String, category: String) = {
+    val basicProbability = featureProbability(feature, category)
+    val totals = featureTotals(feature)
+    println(basicProbability)
+    weightedAverage(1.0, 0.5, totals, basicProbability)
   }
 
   /**
