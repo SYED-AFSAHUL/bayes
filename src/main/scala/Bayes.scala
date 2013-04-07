1c24c7b85ee92087526e8dc15b11849515ce721f
owain lewis <owain@owainlewis.com>
2013-04-07 14:24:32 +0100

    working through

--- src/main/scala/Bayes.scala
+++ src/main/scala/Bayes.scala
@@ -1,5 +1,7 @@
 package bayes
 
+import collection.mutable
+
 class Tokenizer(val text: String) {
 
   val normalize = (text: String) => text.toLowerCase
@@ -8,31 +10,38 @@ class Tokenizer(val text: String) {
 
   def tokenize(): List[String] =
     (words compose normalize)(text).toList
+
+  def ngramTokenizer(n: Int, text: String) = sys.error("TODO")
 }
 
 trait Classifier {
   def classify(text: String)
 }
 
+case class Word(category: String, word: String, count: Int)
+
 object Bayes extends Classifier {
 
-  // Map(category -> Map(word -> count))
-  var wordCount = scala.collection.mutable.Map[String, Map[String, Int]]()
+  val wordCount = mutable.Map[(String, String), Int]()
 
-  var categoryCount = collection.mutable.Map[String, Int]()
+  val categoryCount = mutable.Map[String, Int]()
 
   def incrementCategoryCount(category: String) = {
-    val count = categoryCount.getOrElseUpdate(category, 0)
-    categoryCount = categoryCount.map {
-      case(k, v) => {
-        if (k == category) (k, v + 1)
-        else (k,v)
-      }
-    }
+    val current = categoryCount.getOrElse(category, 0)
+    categoryCount.put(category, 1+current)
   }
 
-  def incrementWordCount(category: String, word: String) =
-    sys.error("todo")
+  def incrementWordCount(category: String, word: String): Unit = {
+    val current = wordCount.get((category, word)).getOrElse(0)
+    wordCount.put((category, word), 1 + current)
+  }
+
+  def wordAppearsInCategory(category: String, word: String): Int = {
+    wordCount.get((category,word)) match {
+      case Some(v: Int) => v
+      case None => 0
+    }
+  }
 
   def classify(text: String) = {
     val tokenizer = new Tokenizer(text)
