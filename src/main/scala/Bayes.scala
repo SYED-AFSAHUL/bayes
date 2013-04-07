e2df12d84b10444480acc4e4dc411439249b9854
owain lewis <owain@owainlewis.com>
2013-04-07 13:09:03 +0100

    basics

--- src/main/scala/Bayes.scala
+++ src/main/scala/Bayes.scala
@@ -1,11 +1,41 @@
 package bayes
 
-object Tokenizer {
+class Tokenizer(val text: String) {
 
-  def normalize(text: String): String = text.toLowerCase
+  val normalize = (text: String) => text.toLowerCase
 
-  def words(text: String): List[String] = words.split(" ")
+  val words = (text: String) => text.split(" ")
 
-  def tokenize(text: String): List[String] = normalize andThen words
+  def tokenize(): List[String] =
+    (words compose normalize)(text).toList
+}
 
+trait Classifier {
+  def classify(text: String)
+}
+
+object Bayes extends Classifier {
+
+  // Map(category -> Map(word -> count))
+  var wordCount = scala.collection.mutable.Map[String, Map[String, Int]]()
+
+  var categoryCount = collection.mutable.Map[String, Int]()
+
+  def incrementCategoryCount(category: String) = {
+    val count = categoryCount.getOrElseUpdate(category, 0)
+    categoryCount = categoryCount.map {
+      case(k, v) => {
+        if (k == category) (k, v + 1)
+        else (k,v)
+      }
+    }
+  }
+
+  def incrementWordCount(category: String, word: String) =
+    sys.error("todo")
+
+  def classify(text: String) = {
+    val tokenizer = new Tokenizer(text)
+    tokenizer.tokenize
+  }
 }
