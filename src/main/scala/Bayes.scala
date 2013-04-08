9d30a62420980aa5995f5216ed919e6e610d04fc
owain lewis <owain@owainlewis.com>
2013-04-08 20:48:19 +0100

    updates tokenizer

--- src/main/scala/Bayes.scala
+++ src/main/scala/Bayes.scala
@@ -2,46 +2,6 @@ package bayes
 
 import collection.mutable
 
-import scala.io.Source.{fromFile}
-
-trait NGramTokenizer {
-
-  def nGrams(n: Int, text: String) = sys.error("TODO")
-
-  def biGrams(text: String) = sys.error("TODO")
-}
-
-class Tokenizer(val text: String) {
-
-  type wordList = List[String]
-
-  val normalize = (text: String) => text.toLowerCase
-  val words = (text: String) => text.split(" ")
-
-  private def readStopWords() = fromFile("stopWords").getLines.toList
-
-  private val badChars = List(',', ';', '-', '?', '\n', '!') map (_.toString)
-
-  def stripPunctuation(text: String): String = {
-    def replaceAll(chars: List[String], t: String): String = chars match {
-      case x::xs => replaceAll(xs, t.replace(x,""))
-      case Nil => t
-    }
-    replaceAll(badChars, text)
-  }
-
-  def filterStopWords(words: wordList): wordList = {
-    val stopWords = readStopWords()
-    words.filter { w => !stopWords.contains(w) }
-  }
-
-  def tokenize(): wordList =
-    (words compose normalize)(text).toList
-
-  def tokenizeFiltered(): wordList =
-    filterStopWords(tokenize)
-}
-
 trait Classifier {
 
   val wordCount = mutable.Map[(String, String), Int]()
---
+++ src/main/scala/Tokenizer.scala
@@ -0,0 +1,50 @@
+package bayes
+
+import scala.io.Source.{fromFile}
+
+trait NGramTokenizer {
+
+  def nGrams(n: Int): List[List[String]]
+
+}
+
+class Tokenizer(val text: String) extends NGramTokenizer {
+
+  type wordList = List[String]
+
+  val normalize = (text: String) => text.toLowerCase
+
+  val words = (text: String) => text.split(" ")
+
+  private def readStopWords() = fromFile("stopWords").getLines.toList
+
+  private val badChars = List(',', ';', '-', '?', '\n', '!') map (_.toString)
+
+  /** Split the text into ngrams */
+  def nGrams(n: Int) = {
+    val t = tokenize()
+    t.sliding(n).toList
+  }
+
+  def biGrams() = nGrams(2)
+
+  def stripPunctuation(text: String): String = {
+    def replaceAll(chars: List[String], t: String): String = chars match {
+      case x::xs => replaceAll(xs, t.replace(x,""))
+      case Nil => t
+    }
+    replaceAll(badChars, text)
+  }
+
+  def filterStopWords(words: wordList): wordList = {
+    val stopWords = readStopWords()
+    words.filter { w => !stopWords.contains(w) }
+  }
+
+  def tokenize(): wordList =
+    (words compose normalize)(text).toList
+
+  def tokenizeFiltered(): wordList =
+    filterStopWords(tokenize)
+}
+
